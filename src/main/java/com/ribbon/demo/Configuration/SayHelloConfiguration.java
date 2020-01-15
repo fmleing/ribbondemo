36ec4d3a630f39018c1be812820d7d934b00d25d
Mengleifeng <fmleing@163.com>
2020-01-15 20:27:41 +0800

    修改 IPing 实现类

--- src/main/java/com/ribbon/demo/Configuration/SayHelloConfiguration.java
+++ src/main/java/com/ribbon/demo/Configuration/SayHelloConfiguration.java
@@ -1,5 +1,10 @@
 package com.ribbon.demo.Configuration;
 
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
@@ -10,7 +15,7 @@ public class SayHelloConfiguration {
 
     @Bean
     public IPing ribbonPing(IClientConfig config) {
-        return new PingUrl();
+        return new PingConstant();
     }
 
     @Bean
