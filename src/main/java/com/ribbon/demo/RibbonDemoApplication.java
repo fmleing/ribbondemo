615b5b143dbd44df3d83ee860ce4faf65f52341d
Mengleifeng <fmleing@163.com>
2020-01-15 20:20:36 +0800

    添加其他配置

--- src/main/java/com/ribbon/demo/RibbonDemoApplication.java
+++ src/main/java/com/ribbon/demo/RibbonDemoApplication.java
@@ -1,5 +1,8 @@
 package com.ribbon.demo;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RibbonDemoApplication {
