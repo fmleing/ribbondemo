80db68652032cc9075de056f9fcf1262364f8d93
Mengleifeng <fmleing@163.com>
2020-01-15 20:30:18 +0800

    Controller 调用逻辑完成

--- src/main/java/com/ribbon/demo/controller/ConsumerController.java
+++ src/main/java/com/ribbon/demo/controller/ConsumerController.java
@@ -2,7 +2,11 @@ package com.ribbon.demo.controller;
 
 import com.ribbon.demo.Configuration.SayHelloConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,11 +14,18 @@ import org.springframework.web.client.RestTemplate;
 @RibbonClient(name = "say-hello", configuration = SayHelloConfiguration.class)
 public class ConsumerController {
 
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
     @Autowired
     RestTemplate restTemplate;
 
-
-    public String helloConsumer(){
-
+    @RequestMapping("/hi")
+    private String helloConsumer(@RequestParam(value="name", defaultValue="Artaban") String name){
+        String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
+        return String.format("%s, %s!", greeting, name);
     }
 }
